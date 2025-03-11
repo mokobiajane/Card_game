e8700c7090a22146b65b4890a70f59b3a11bd723
mokobiajane <st135735@student.spbu.ru>
2025-03-11 14:39:44 +0300

    issues with CI problems fixed

---
+++ include/CardGame/AIplayer.hpp
@@ -0,0 +1,10 @@
+#ifndef CARD_HPP
+#define CARD_HPP
+
+class Card {
+public:
+    Card() = default;
+    void print() const;
+};
+
+#endif // CARD_HPP
---
+++ src/AIplayer.cpp
@@ -0,0 +1,6 @@
+#include <CardGame/Card.hpp>
+#include <iostream>
+
+void Card::print() const {
+    std::cout << "This is a card!" << std::endl;
+}
