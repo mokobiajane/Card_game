8a86ed0990d5698041fbc06d8950beeb264861e4
mokobiajane <st135735@student.spbu.ru>
2025-04-21 15:18:26 +0300

    added build to gitignore

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test([=[CardGameTests]=] "/home/jane/Desktop/Card_game/build/runTests")
-set_tests_properties([=[CardGameTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/jane/Desktop/Card_game/CMakeLists.txt;40;add_test;/home/jane/Desktop/Card_game/CMakeLists.txt;0;")
+set_tests_properties([=[CardGameTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/jane/Desktop/Card_game/CMakeLists.txt;41;add_test;/home/jane/Desktop/Card_game/CMakeLists.txt;0;")
